49cbf9a8dc43055e6f7cecb2bb576bb104e718bb
SangWon7242 <gotkdqja@gmail.com>
2025-03-29 11:44:23 +0900

    requests 설치 및 테스트

---
+++ tests/test.py
@@ -0,0 +1,17 @@
+# 테스트 환경
+
+import requests
+
+# GET 요청 (데이터 조회)
+resp = requests.get('https://www.naver.com/')
+
+# 상태 코드 확인
+print(resp.status_code)  # 200: 성공, 404: 찾을 수 없음, 500: 서버 에러
+
+# 응답 내용 확인
+if(resp.status_code == 200):
+  # print(resp.url) # url 정보
+  # print(resp.json) # json 데이터
+  print(resp.text)  # HTML 문서를 가져옴
+else:
+  print("응답에 실패했습니다.")
