7aab722fb5efc0e1747023ba05759e601bc6e0f3
SangWon7242 <gotkdqja@gmail.com>
2025-03-30 10:38:16 +0900

    네이버 뉴스기사 url 가져오기

--- src/main.py
+++ src/main.py
@@ -1,1 +1,19 @@
 # 실제 구현 환경
+
+import requests
+from bs4 import BeautifulSoup
+
+
+# GET 요청 (데이터 조회)
+resp = requests.get('https://news.naver.com/section/103')
+
+# 상태 코드 확인
+print(resp.status_code)  # 200: 성공, 404: 찾을 수 없음, 500: 서버 에러
+
+# 응답 내용 확인
+if(resp.status_code == 200):
+  # print(resp.url) # url 정보
+  # print(resp.json) # json 데이터
+  print(resp.text)  # HTML 문서를 가져옴
+else:
+  print("응답에 실패했습니다.")
