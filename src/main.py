e6e75530ce2bdb60e6e79dee14a43319fccbb79a
SangWon7242 <gotkdqja@gmail.com>
2025-03-30 11:10:05 +0900

    관심 키워드로 뉴스 제목 추출

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 
 
 # GET 요청 (데이터 조회)
-resp = requests.get('https://news.naver.com/section/103')
+resp = requests.get('https://news.naver.com/section/104')
 
 # 상태 코드 확인
 print(resp.status_code)  # 200: 성공, 404: 찾을 수 없음, 500: 서버 에러
@@ -37,3 +37,25 @@ print("== 뉴스 기사 제목 출력 ==")
 for i, title in enumerate(news_title_text):
   no = i + 1
   print(f"{no} : {title}")
+  
+# 특정 키워드로 제목 검색  
+# title.find(keyword) -> 결과값이 0아니면 -1
+# 0인 경우에 내가 원하는 키워드가 존재한다.
+# -1인 경우에는 내가 원하는 키워드가 존재하지 않는다.
+
+print("== 키워드로 뉴스 기사 검색 ==")
+find_keyword_list = []
+keyword = '트럼프'
+for i, title in enumerate(news_title_text):
+  no = i + 1    
+  
+  if title.find(keyword) != -1:
+    # print(f"{no} : {title}")
+    find_keyword_list.append(title)
+    
+print(find_keyword_list) # 특정 키워드로 걸러진 데이터가 리스트에 저장
+
+# 특정 키워드를 가진 뉴스 기사 제목을 반복문을 이용한 출력
+print("== 키워드 뉴스 제목 리스트 순회 ==")
+for i, title in enumerate(find_keyword_list):
+  print(f"{i + 1} : {title}")
